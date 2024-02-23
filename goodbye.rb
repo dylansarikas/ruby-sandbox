ca21f0b15283b271927117d8f9734b222f8030f9
Dylan Sarikas <97060448+dylansarikas@users.noreply.github.com>
2024-02-23 19:58:17 +0000

    created ruby file for testing

---
+++ goodbye.rb
@@ -0,0 +1,3 @@
+another_string = "oh my ga"
+
+pp another_string
---
+++ howdy.rb
@@ -0,0 +1,10 @@
+require "active_support/all"
+
+my_string = "Blobs are good"
+pp my_string
+
+require "./goodbye.rb"
+
+pp 31.ordinalize
+pp "ruby".pluralize
+pp "hi"
